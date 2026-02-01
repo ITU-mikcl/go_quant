fab7a32ecb592b6e0eedef3e042614ebb41ab1a4
Mikkel Clausen <mikcl@itu.dk>
2026-02-01 16:00:49 +0100

    Get 10 years of bars

--- go.mod
+++ go.mod
@@ -1,3 +1,12 @@
 module project/go_quant
 
 go 1.25.6
+
+require (
+	cloud.google.com/go v0.123.0 // indirect
+	github.com/alpacahq/alpaca-trade-api-go/v3 v3.9.0 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/josharian/intern v1.0.0 // indirect
+	github.com/mailru/easyjson v0.9.1 // indirect
+	github.com/shopspring/decimal v1.4.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,7 +1,56 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/alpacahq/alpaca-trade-api-go/v3/marketdata"
+	"github.com/joho/godotenv"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	err := godotenv.Load(".env")
+	if err != nil {
+		panic("Error loading .env file")
+	}
+
+	client := marketdata.NewClient(marketdata.ClientOpts{
+		APIKey:    os.Getenv("APCA_API_KEY_ID"),
+		APISecret: os.Getenv("APCA_API_SECRET_KEY"),
+		BaseURL:   "https://data.alpaca.markets",
+	})
+
+	latestTradingDay := getLatestTradingDay()
+
+	request := marketdata.GetBarsRequest{
+		TimeFrame: marketdata.OneDay,
+		Start:     latestTradingDay.AddDate(-10, 0, 0),
+		End:       latestTradingDay,
+	}
+
+	bars, err := client.GetBars("GOOGL", request)
+	if err != nil {
+		panic(err)
+	}
+	for _, bar := range bars {
+		fmt.Printf("%+v\n", bar)
+	}
+}
+
+// getLatestTradingDay returns today if it's a weekday, or the last Friday if today is a weekend
+func getLatestTradingDay() time.Time {
+	now := time.Now()
+	weekday := now.Weekday()
+
+	// If today is Saturday or Sunday, go back to Friday
+	switch weekday {
+	case time.Saturday:
+		return now.AddDate(0, 0, -1)
+	case time.Sunday:
+		return now.AddDate(0, 0, -2)
+	}
+
+	// Otherwise, today is a trading day (Mon-Fri)
+	return now
 }
